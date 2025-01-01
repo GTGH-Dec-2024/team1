55c0ba5f8395aa67643b4ecfaeff8e008dad31f8
argirostavridou <stavridarg@gmail.com>
2025-01-01 17:07:47 +0200

    Comments added
    Some comment have been added.
    Greeklish comment are just temporary and will be deleted soon.

--- Team1_Project/src/Reservation.java
+++ Team1_Project/src/Reservation.java
@@ -1,19 +1,26 @@
-
+/* The class Reservation stores information about the reservation 
+ * that a visitor has done for a specific event.
+ */
 import java.util.Date;
 
 public class Reservation {
-	private Visitor visitor;  
-    private Event event;      
+	// The visitor who has made the reservation
+	private Visitor visitor; // connection with the class Visitor 
+    // The event to which the reservation has been made
+	private Event event; // connection with the class Event
  
   
-    
+    /* Constructor.
+     * visitor: the visitor who makes the reservation to the event.
+     * event: the event for which the reservation is being made.
+     */
     public Reservation(Visitor visitor, Event event) {
         this.visitor = visitor;
         this.event = event;
         
     }
 
-    
+    //Getters and Setters for the fields Visitor and Reservation
     public Visitor getVisitor() {
         return visitor;
     }
@@ -29,7 +36,8 @@ public class Reservation {
     public void setEvent(Event event) {
         this.event = event;
     }
-
+     
+    // Method which shows the information of the reservation.
     //mhpos na mhn emfanizei oles perites plirofories poios visitor(surname...) title wra hmerominia location 
     public void displayReservationDetails() {
         System.out.println("Reservation Details:");
--- Team1_Project/src/Visitor.java
+++ Team1_Project/src/Visitor.java
@@ -1,3 +1,11 @@
+/* The class Visitor rempresents one visitor who attends an event.
+ * 1)The visitor can search the events that he would like to attend based on specific
+ * criteria such as: The date, the location , the theme of the event (mipos kai kati allo?)
+ * 2)After that, he can do a reservation to the Event that he wants to attend.
+ * 3)If he changes his mind, he can cancel the reservation that he has done.
+ */
+
+/* The class Visitor inherits the abstract class User and he has also an email. */
 import java.util.ArrayList;
 import java.util.List;
 
@@ -5,19 +13,21 @@ public class Visitor extends User {
 	
     private String email;
 
-    private List<Reservation> reservations;
-
-	
+    private List<Reservation> reservations; // List that with objects of the class Reservation
+    // The list Reservation is the storing area where each Visitor can store his reservations.
+    
+	// Constructor
     public Visitor(String name, String surname, String email) {
-        super(name, surname);  
+        super(name, surname);  // Calls the constructor of the class User
         this.reservations = new ArrayList<>();
         this.email = new email;
     }
-
+   /* The public method  searchEvents of the Visitor class calls the
+    * findEvents method of the class eventManager. */
    //na thn kanw void? kai oxi lista//prepei na dimiourgisw antikeimeno eventmanager
     // gia na borw na kalw ton event manager, tha pernei day-month year
     public List<Event> searchEvents(String location, String theme, 
-                                    int day, int month, int year,) {
+                                    int day, int month, int year) {
         return eventManager.findEvents(location, theme, day, month, year, hour);
     }
     
@@ -27,19 +37,20 @@ public class Visitor extends User {
     // na thn kanw void kai na epistrefei apla to minima me system out print
     // prepei na vevaiwtho oti den yparxei reservation, des me boolean true false.
     public String makeReservation(Event event) {
-        
+        //Elegxos an uparxei hdh krathsh gia thn idia ekdilwsi
         for (Reservation reservation : reservations) {
             if (reservation.getEvent().equals(event)) {
                 return "Reservation already exists.";
             }
         }
 
-        
+        //Dimiourgia kai prosthiki kratisis
         Reservation newReservation = new Reservation(this, event);
         reservations.add(newReservation);
         return "Reservation successful: " + event.getTitle();
     }
     
+    //akurwnei mia kratisi gia tin ekdilwsi
     //tha tin kanw kai aythn void, des methodo contains
     public String cancelReservation(Event event) {
         for (Reservation reservation : reservations) {
@@ -51,7 +62,8 @@ public class Visitor extends User {
         return "reservation for event not found: " + event.getTitle();
     }
     
-    
+    // an den xreiazetai tha tin diagrapsw
+    // A list which stores and returns all the reservations of the Visitor
     public List<Reservation> getReservations() {
         return reservations;
     }
