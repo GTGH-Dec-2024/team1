c1ca71fc34222f45c39ab29c272d01ebf6259bc5
Dimitra Moraiti <moraitdi@gmail.com>
2025-01-03 13:49:32 +0200

    Added 3 more events in Main.java class

--- Team1_Project/src/Main.java
+++ Team1_Project/src/Main.java
@@ -16,27 +16,27 @@ public class Main {
 		Event ev1 = new Event("Cultural Night of Thessaloniki", 
 				"Traditional Music and Dances of Macedonia",
 				" A mesmerizing evening filled with traditional dances, music, and flavors of Macedonia, featuring local dance groups and musical ensembles.", 
-				"Aristotelous Square", 150, 15, 6, 2025, 8, 00, 3, o1);
+				"Aristotelous Square", 25, 15, 6, 2025, 20, 00, 3, o1);
 		
 		Event ev2 = new Event("Thessaloniki Taste Festival", 
 				"Showcasing Local Gastronomy with Restaurants and Producers", 
 				"A journey into the flavors of Thessaloniki, with exhibitions of local products, cooking demonstrations, and tastings by renowned chefs.",
-				"Nikis Avenue Pedestrian Zone", 200, 10, 9, 2025, 6, 00, 4, o2);
+				"Nikis Avenue Pedestrian Zone", 30, 10, 9, 2025, 18, 00, 4, o2);
 		
 		Event ev6 = new Event("Thessaloniki Marathon Festival",
 				"Running Through the History and Beauty of Thessaloniki",
 				"A city-wide marathon event that welcomes runners of all levels to explore Thessaloniki’s landmarks while promoting a healthy and active lifestyle. Includes live music and food stands.",
-				"Start and finish at White Tower Square", 10000, 20, 10, 2025, 9, 30, 6, o1);
+				"Start and finish at White Tower Square", 15, 20, 10, 2025, 21, 30, 6, o1);
 		
 		Event ev7 = new Event("Open-Air Cinema Night", 
 				"Classic Movies Under the Stars", 
 				"A relaxing evening of cinema in an open-air setup, featuring classic movies on a giant screen. Bring your blanket and enjoy popcorn under the night sky.", 
-				"Seikh Sou Forest Park", 800, 12, 7, 2025, 8, 30, 3, o2);
+				"Seikh Sou Forest Park", 20, 12, 7, 2025, 20, 30, 3, o2);
 		
 		Event ev8 = new Event("Thessaloniki Tech Fair", 
 				"Innovations Shaping the Future", 
 				"An exhibition of cutting-edge technologies and startups, with hands-on demonstrations, networking opportunities, and guest speakers from the tech world.", 
-				"Thessaloniki International Fairgrounds (TIF HELEXPO)", 5000, 15, 11, 2025, 10,30, 8, o2);
+				"Thessaloniki International Fairgrounds (TIF HELEXPO)", 25, 15, 11, 2025, 15 ,30, 8, o2);
 		
 		
 
