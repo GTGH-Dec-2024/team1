e07ca435d873e1593aef6e47f624650f45cc621f
em-maeva <evamar259@gmail.com>
2025-01-03 13:14:34 +0200

    added organizers to main - minor change on organzier class

--- Team1_Project/src/Main.java
+++ Team1_Project/src/Main.java
@@ -3,7 +3,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Organizer o1 = new Organizer("George", "Georgidis","A111","An organizer that likes events");
+		Organizer o2 = new Organizer("Maria", "Maraki","A222"," ");
+		Organizer o3 = new Organizer("Petros", "Petridis","A333","The best organizer");
+		Organizer o4 = new Organizer("Zoe", "Zoedi", "A444", "A new event organizer!");
+	
+		
+		
+		
+		
+		
 	}
 
 }
