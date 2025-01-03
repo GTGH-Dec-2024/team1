e6eac610d362ab4be240b60668088dd1e42cde7e
Dimitra Moraiti <moraitdi@gmail.com>
2025-01-03 13:38:33 +0200

    Changed structure of constructors in Main.java

--- Team1_Project/src/Main.java
+++ Team1_Project/src/Main.java
@@ -3,7 +3,16 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		Employee em1 = new Employee("Maria ", "Papadopoulou", "maria.papadop@gmail.com");
+		Employee em2 = new Employee("Dimitris ", "Konstantinidis", "dimitris.kons@gmail.com");
+		Employee em3 = new Employee("Giorgos  ", "Nikolaidis", " giorgos.nikolaid@example.com");
+
 		
+		Organizer o1 = new Organizer("George", "Georgidis","A111","An organizer that likes events");
+		Organizer o2 = new Organizer("Maria", "Maraki","A222"," ");
+		Organizer o3 = new Organizer("Petros", "Petridis","A333","The best organizer");
+		Organizer o4 = new Organizer("Zoe", "Zoedi", "A444", "A new event organizer!");
+	
 		Event ev1 = new Event("Cultural Night of Thessaloniki", 
 				"Traditional Music and Dances of Macedonia",
 				" A mesmerizing evening filled with traditional dances, music, and flavors of Macedonia, featuring local dance groups and musical ensembles.", 
@@ -13,20 +22,6 @@ public class Main {
 				"Showcasing Local Gastronomy with Restaurants and Producers", 
 				"A journey into the flavors of Thessaloniki, with exhibitions of local products, cooking demonstrations, and tastings by renowned chefs.",
 				"Nikis Avenue Pedestrian Zone", 200, 10, 9, 2025, 6, 00, 4, o2);
-		
-		Employee em1 = new Employee("Maria ", "Papadopoulou", "maria.papadop@gmail.com");
-		Employee em2 = new Employee("Dimitris ", "Konstantinidis", "dimitris.kons@gmail.com");
-		Employee em3 = new Employee("Giorgos  ", "Nikolaidis", " giorgos.nikolaid@example.com");
-
-
-		// TODO Auto-generated method stub
-		
-		Organizer o1 = new Organizer("George", "Georgidis","A111","An organizer that likes events");
-		Organizer o2 = new Organizer("Maria", "Maraki","A222"," ");
-		Organizer o3 = new Organizer("Petros", "Petridis","A333","The best organizer");
-		Organizer o4 = new Organizer("Zoe", "Zoedi", "A444", "A new event organizer!");
-	
-		
 		
 		
 		
