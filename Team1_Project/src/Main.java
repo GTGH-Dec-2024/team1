5b73cb937dec64be2406fa15dd59fbc72ec71d38
em-maeva <evamar259@gmail.com>
2025-01-06 23:45:04 +0200

    Removed duplicates

--- Team1_Project/src/Main.java
+++ Team1_Project/src/Main.java
@@ -93,39 +93,7 @@ public class Main {
 				"Thessaloniki International Fairgrounds (TIF HELEXPO)", 25, 15, 11, 2025, 15 ,30, 8, o3);
 
 		
-        List<Visitor> visitors = new ArrayList<>();
-
-        Visitor visitor1 = new Visitor("Giorgos", "Papadopoulos", "visitor1@gmail.com");
-        visitors.add(visitor1);
-
-        Visitor visitor2 = new Visitor("Maria", "Ioannidou", "visitor2@gmail.com");
-        visitors.add(visitor2);
-
-        Visitor visitor3 = new Visitor("Kostas", "Alexiou", "visitor3@gmail.com" );
-        visitors.add(visitor3);
-
-        Visitor visitor4 = new Visitor("Panagiotis", "Konstantinidis", "visitor4@gmail.com");
-        visitors.add(visitor4);
-
-        Visitor visitor5 = new Visitor("Katerina", "Antoniou","visitor5@gmail.com");
-        visitors.add(visitor5);
-
-        Visitor visitor6 = new Visitor("Nikos", "Papakonstantinou","visitor6@gmail.com" );
-        visitors.add(visitor6);
-
-        Visitor visitor7 = new Visitor("Maria", "Oikonomidou", "visitor7@gmail.com");
-        visitors.add(visitor7);
-
-        Visitor visitor8 = new Visitor("Nikos", "Pantelidis","visitor8@gmail.com" );
-        visitors.add(visitor8);
-
-        Visitor visitor9 = new Visitor("Anastasia", "Georgiou","visitor9@gmail.com" );
-        visitors.add(visitor9);
-
-        Visitor visitor10 = new Visitor("Dimitra", "Aggelidou", "visitor10@gmail.com");
-        visitors.add(visitor10);
-        
-        
+      
         //Organizers are creating approval requests for their events
         ApprovalRequest registerRequest1 =  o3.registerRequest(ev3);      
         ApprovalRequest registerRequest2 = o2.registerRequest(ev2);
@@ -151,9 +119,7 @@ public class Main {
     //    visitor1.searchEvent(12, 7, 2025, "Thessaloniki International Fairgrounds (TIF HELEXPO)", "Thessaloniki Tech Fair");
    
         System.out.println(EventManager.getInstance().getEvents());
-       
-    }
-}
+
 
        
 		// Egrafi parakolouthisis endilwshs toulaxiston se mia ekdilosi gia olous tous episkeptes
