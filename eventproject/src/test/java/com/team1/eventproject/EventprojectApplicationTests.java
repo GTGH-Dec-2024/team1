29ccaeab16dece9add3442ee43ac8f1e24ef62d4
Theodore Maikantis <teomaik19@gmail.com>
2025-01-08 17:19:12 +0200

    Added a Spring Boot Project's base files

---
+++ eventproject/src/test/java/com/team1/eventproject/EventprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team1.eventproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
