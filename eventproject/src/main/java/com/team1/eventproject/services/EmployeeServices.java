e5dbee9f73250b30df5f2dda1718c3213aa8931e
em-maeva <evamar259@gmail.com>
2025-01-10 14:45:48 +0200

    More service classes
    Made a first implementation of 3 service classes, EmployeeService, OrganizerService and VisitorService

--- eventproject/src/main/java/com/team1/eventproject/services/EmployeeServices.java
+++ eventproject/src/main/java/com/team1/eventproject/services/EmployeeServices.java
@@ -1,12 +1,42 @@
 package com.team1.eventproject.services;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import com.team1.eventproject.entities.Employee;
 
 public class EmployeeServices {
 		private ArrayList<Employee> allEmployees;
 		
-	//public
+		public List<Employee> addEmployee (int id, String name, String surname, String email)
+		{
+			Employee temp = new Employee(id, name, surname, email);
+			if (!allEmployees.contains(temp))
+			{
+				allEmployees.add(temp);
+			}
+			else
+			{
+				System.out.println("The employee: " +temp.getName()+ temp.getSurname()+
+						" has already been added");
+			}
+			return allEmployees;
+		}
+		
+		
+		
+		public Employee getEmployeeByID(int id)
+		{
+			for (Employee employee : allEmployees) 
+			{
+		        if (employee.getId() == id) 
+		        {
+		            return employee;
+		        }
+		    }
+		    return null; 
+		}
+		
+		
 
 }
---
+++ eventproject/src/main/java/com/team1/eventproject/services/VisitorServices.java
@@ -0,0 +1,5 @@
+package com.team1.eventproject.services;
+
+public class VisitorServices {
+
+}
